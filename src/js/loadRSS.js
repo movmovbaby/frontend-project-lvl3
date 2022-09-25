c3efb1c34fe8715b3b383cdb598d938b5603b7b0
Nikolai Ochirov <dubdubman@ya.ru>
2022-09-25 23:25:31 +0300

    fix 'Expect proxified url to have "get" pathname' message

--- src/js/loadRSS.js
+++ src/js/loadRSS.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default (url) => {
-  const proxyURL = `https://allorigins.hexlet.app/raw?url=${url}&disableCache=true`;
+  const proxyURL = `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`;
   return axios.get(proxyURL);
 };
