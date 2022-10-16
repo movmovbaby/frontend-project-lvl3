2e7e1420e5ff0e31fb2f26f9c5fc1d2b7a6c6253
Kolya <dubdubman@ya.ru>
2022-10-16 23:22:37 +0300

    fix on mentor's comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,100 +1,111 @@
 import _ from 'lodash';
 import * as yup from 'yup';
+import i18n from 'i18next';
+import resources from './locales/index.js';
 import view from './view.js';
 import { loadRSS, parserRSS, updateRSS } from './rssUtils.js';
 
-export default (i18nInstance) => {
-  const elements = {
-    form: document.querySelector('.rss-form'),
-    feedback: document.querySelector('.feedback'),
-    input: document.getElementById('url-input'),
-    submitButton: document.querySelector('button[type="submit"]'),
-    feedsContainer: document.querySelector('.feeds'),
-    postsContainer: document.querySelector('.posts'),
-    modal: document.querySelector('#modal'),
-  };
+export default () => {
+  const i18nInstance = i18n.createInstance();
+  i18nInstance
+    .init({
+      lng: 'ru',
+      debug: false,
+      resources,
+    })
+    .then(() => {
+      const elements = {
+        form: document.querySelector('.rss-form'),
+        feedback: document.querySelector('.feedback'),
+        input: document.getElementById('url-input'),
+        submitButton: document.querySelector('button[type="submit"]'),
+        feedsContainer: document.querySelector('.feeds'),
+        postsContainer: document.querySelector('.posts'),
+        modal: document.querySelector('#modal'),
+      };
 
-  yup.setLocale({
-    string: {
-      url: 'form.error.urlInvalid',
-    },
+      yup.setLocale({
+        string: {
+          url: 'form.error.urlInvalid',
+        },
 
-    mixed: {
-      required: 'form.error.urlRequired',
-      notOneOf: 'form.error.urlDuplicate',
-    },
-  });
+        mixed: {
+          required: 'form.error.urlRequired',
+          notOneOf: 'form.error.urlDuplicate',
+        },
+      });
 
-  const urlSchema = yup
-    .string()
-    .required()
-    .url();
+      const urlSchema = yup
+        .string()
+        .required()
+        .url();
 
-  const validateURL = (url, urls) => urlSchema.notOneOf(urls).validate(url);
+      const validateURL = (url, urls) => urlSchema.notOneOf(urls).validate(url);
 
-  const initialState = {
-    feeds: [],
-    posts: [],
-    urls: [],
-    form: {
-      error: null,
-      valid: null,
-      processState: 'filling',
-    },
-    uiState: {
-      visitedPosts: new Set(),
-      dataIDForModal: null,
-    },
-  };
+      const initialState = {
+        feeds: [],
+        posts: [],
+        urls: [],
+        form: {
+          error: null,
+          valid: null,
+          processState: 'filling',
+        },
+        uiState: {
+          visitedPosts: new Set(),
+          dataIDForModal: null,
+        },
+      };
 
-  const state = view(initialState, elements, i18nInstance);
+      const state = view(initialState, elements, i18nInstance);
 
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const url = formData.get('url');
 
-    validateURL(url, state.urls)
-      .then((validUrl) => {
-        state.form.valid = true;
-        state.urls.push(url);
-        state.form.processState = 'sending';
-        return loadRSS(validUrl);
-      })
-      .then((rss) => {
-        state.form.processState = 'sent';
-        const { feed, posts } = parserRSS(rss);
-        const feedID = _.uniqueId();
-        const rssFeed = { ...feed, id: feedID, url };
-        const rssPosts = posts.map((post) => ({ ...post, id: _.uniqueId(), feedID }));
-        state.feeds = [rssFeed, ...state.feeds];
-        state.posts = [...rssPosts, ...state.posts];
-        state.form.processState = 'dataLoaded';
-      })
-      .catch((error) => {
-        state.form.valid = error.name !== 'ValidationError';
-        if (error.name === 'ValidationError') {
-          state.form.error = error.message;
-        } else if (error.rssInvalid) {
-          state.form.error = 'form.error.rssInvalid';
-        } else if (error.name === 'AxiosError') {
-          state.form.error = 'form.error.networkError';
+        validateURL(url, state.urls)
+          .then((validUrl) => {
+            state.form.valid = true;
+            state.urls.push(url);
+            state.form.processState = 'sending';
+            return loadRSS(validUrl);
+          })
+          .then((rss) => {
+            state.form.processState = 'sent';
+            const { feed, posts } = parserRSS(rss);
+            const feedID = _.uniqueId();
+            const rssFeed = { ...feed, id: feedID, url };
+            const rssPosts = posts.map((post) => ({ ...post, id: _.uniqueId(), feedID }));
+            state.feeds = [rssFeed, ...state.feeds];
+            state.posts = [...rssPosts, ...state.posts];
+            state.form.processState = 'dataLoaded';
+          })
+          .catch((error) => {
+            state.form.valid = error.name !== 'ValidationError';
+            if (error.name === 'ValidationError') {
+              state.form.error = error.message;
+            } else if (error.rssInvalid) {
+              state.form.error = 'form.error.rssInvalid';
+            } else if (error.name === 'AxiosError') {
+              state.form.error = 'form.error.networkError';
+            }
+          });
+      });
+
+      elements.postsContainer.addEventListener('click', (e) => {
+        const clicked = e.target;
+        if (clicked.closest('a')) {
+          const { id } = clicked.dataset;
+          state.uiState.visitedPosts.add(id);
+        }
+        if (clicked.closest('button')) {
+          const { id } = clicked.dataset;
+          state.uiState.visitedPosts.add(id);
+          state.uiState.dataIDForModal = id;
         }
       });
-  });
 
-  elements.postsContainer.addEventListener('click', (e) => {
-    const clicked = e.target;
-    if (clicked.closest('a')) {
-      const { id } = clicked.dataset;
-      state.uiState.visitedPosts.add(id);
-    }
-    if (clicked.closest('button')) {
-      const { id } = clicked.dataset;
-      state.uiState.visitedPosts.add(id);
-      state.uiState.dataIDForModal = id;
-    }
-  });
-
-  setTimeout(() => updateRSS(state), 5000);
+      setTimeout(() => updateRSS(state), 5000);
+    });
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,5 @@
 import '../scss/styles.scss';
 import 'bootstrap';
-import i18n from 'i18next';
 import app from './index.js';
-import resources from './locales/index.js';
 
-const i18nInstance = i18n.createInstance();
-i18nInstance
-  .init({
-    lng: 'ru',
-    debug: false,
-    resources,
-  })
-  .then(app(i18nInstance))
-  .catch((e) => {
-    throw (e);
-  });
+app()
